565eac915a83b825a2100aaffd59e57160b070c3
bryan-beck <bdbeck89@gmail.com>
2021-09-14 08:22:50 -0700

    another update

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require ('express');
 const path = require("path");
-const Rollbar = require("rollbar");
+const Rollbar = require('rollbar');
 
+
+let rollbar = new Rollbar({
+    accesstoken: "5e74406e3924493c8e82ac1a8639d856",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 const app = express();
 
 app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "/public/index.html"));
+        rollbar.info("HTML file served successfully!");
     });
 
 const port = process.env.PORT || 4545;
+
 app.listen(port, () => {
     console.log(`they're taking the hobbitss to ${port}!`);
     });
