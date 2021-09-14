f4dd5a46f23b5f49015eee182bd35c7db4745536
bryan-beck <bdbeck89@gmail.com>
2021-09-14 09:09:02 -0700

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let rollbar = new Rollbar({
 })
 const students = [];
 const app = express();
-
+app.use(express.json());
 app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "/public/index.html"));
         rollbar.info("HTML file served successfully!");
